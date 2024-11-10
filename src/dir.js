3730608de6fff4bd9d76dd949a7451547c063c21
anzonyg <anzonyg@live.com>
2024-11-10 14:57:53 -0600

    tercer commit

--- src/dir.js
+++ src/dir.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:4000/tasks/';
+/* const API_BASE_URL = 'http://localhost:4000/tasks/'; */
 const API_BASE_URL = 'https://tesis-backend2.onrender.com/tasks/';
 
 export default API_BASE_URL;
